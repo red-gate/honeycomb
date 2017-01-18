042794a4320a7325e720629bcf4f434da11a543f
Edward Casbon <edward@edwardcasbon.co.uk>
2017-01-18 12:24:11 +0000

    Refactored GitHub commits JS

--- src/assets/js/site/github-commits.js
+++ src/assets/js/site/github-commits.js
@@ -1,6 +1,62 @@
+const commitLength = 5;
+const sources = [
+    {
+        "label": "Honeycomb website",
+        "repo": "https://api.github.com/repos/red-gate/honeycomb/commits?sha=v2-2017",
+        // "repo": "https://api.github.com/repos/red-gate/honeycomb/commits",
+        "selector": ".js-github-honeycomb-website"
+    },
+    {
+        "label": "Honeycomb web toolkit",
+        "repo": "https://api.github.com/repos/red-gate/honeycomb-web-toolkit/commits",
+        "selector": ".js-github-honeycomb-web-toolkit"
+    }
+];
+
 const init = () => {
-    console.log("Init for GitHub commits");
+
+    // Loop through sources, get commit history and print to the page.
+    for (let i = 0; i < sources.length; i++) {
+        let source = sources[i];
+        fetchData(source.repo, (data) => {
+            printHistory(data, source.selector);
+        });
+    }
 };
+
+// Fetch the data from the supplied repo, and fire off the callback.
+const fetchData = (repo, callback) => {
+    $.ajax({
+      url: repo,
+      dataType: 'json',
+      success: function(data) {
+        callback.call(undefined, data);
+      }
+    });
+};
+
+// Print the history to the page.
+const printHistory = (data, selector) => {
+    let html = "";
+    for (let i = 0; i < commitLength; i++) {
+        let commit = data[i];
+        let url = commit.html_url;
+        let msg = commit.commit.message.split("\n")[0];
+        let author = commit.commit.author.name;
+        let datetime = new Date(commit.commit.author.date).toString().split(" ");
+
+        let markup = `
+            <li>
+                <h3 class="spaced-bottom--none"><a href="${url}">${msg}</a></h3>
+                <small>${author}, ${datetime[0]} ${datetime[2]} ${datetime[1]}, ${datetime[4]}</small>
+            </li>
+        `;
+
+        html += markup;
+    }
+
+    document.querySelector(selector).innerHTML = `<ul>${html}</ul>`;
+}
 
 export default {
     init
