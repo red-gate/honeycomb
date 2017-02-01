62edc8d2b51dee8f41e8eeb9d00c03ceb2fa2022
Edward Casbon <edward@edwardcasbon.co.uk>
2017-02-01 10:05:15 +0000

    Bug fix for nav active state JavaScript.

--- src/assets/js/site/nav-active-state.js
+++ src/assets/js/site/nav-active-state.js
@@ -5,8 +5,9 @@ const init = (uri) => {
 
 const secondaryNav = (uri) => {
     const nav = document.querySelector(".nav-bar");
-    const links = nav.querySelectorAll("a");
+    if(!nav) return;
 
+    const links = nav.querySelectorAll("a");
     links.forEach((link) => {
         if(uri.match(link.getAttribute("href"))) {
             link.parentElement.classList.add("active");
