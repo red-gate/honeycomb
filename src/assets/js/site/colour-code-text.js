1a58e00c3c6388a236f4018a82138ee0f03437a0
Edward Casbon <edward@edwardcasbon.co.uk>
2017-02-22 10:38:08 +0000

    Added colours

--- src/assets/js/site/colour-code-text.js
+++ src/assets/js/site/colour-code-text.js
@@ -1,36 +1,21 @@
-var Site = (function(){
+/*
+ * Replace the colour codes with copy'able text, rather than pseudo element.
+ */
+export default () => {
+    const els = document.querySelectorAll('[class*=" color-code--"]');
+    for(let i=0; i<els.length; i++) {
+        let el = els[i];
 
-    var init = function init() {
-        colourCodeText();
-    };
+        // Get the colour code from the computed style.
+        let code = window.getComputedStyle(el, '::after').content.replace(/\"/gi, '');
 
-    /*
-     * Replace the colour codes with copy'able text, rather than pseudo element.
-     */
-    var colourCodeText = function colourCodeText() {
-        var els = document.querySelectorAll('[class*=" color-code--"]');
-        for(var i=0; i<els.length; i++) {
-            var el = els[i];
+        // Add a new p element with the colour code as the content.
+        let p = document.createElement('p');
+        let content = document.createTextNode(code);
+        p.appendChild(content);
+        el.appendChild(p);
 
-            // Get the colour code from the computed style.
-            var code = window.getComputedStyle(el, '::after').content.replace(/\"/gi, '');
-
-            // Add a new p element with the colour code as the content.
-            var p = document.createElement('p');
-            var content = document.createTextNode(code);
-            p.appendChild(content);
-            el.appendChild(p);
-
-            // Update class name to remove ::after pseudo element.
-            el.className = el.className.replace('color-code--', 'color-no-code--');
-        }
-    };
-
-    return {
-        init: init
-    };
-})();
-
-document.addEventListener('DOMContentLoaded', function() {
-    Site.init();
-});
+        // Update class name to remove ::after pseudo element.
+        el.className = el.className.replace('color-code--', 'color-no-code--');
+    }
+};
