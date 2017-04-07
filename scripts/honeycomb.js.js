24fe5b61f382e913b561e10cd40a575dd34eb10f
Edward Casbon <edward@edwardcasbon.co.uk>
2017-04-07 16:21:10 +0100

    Added build script for copying vendor JS

---
+++ scripts/honeycomb.js.js
@@ -0,0 +1,22 @@
+const fs = require("fs-extra");
+const path = require("path");
+const glob = require("glob");
+
+const vendorDir = 'dist/assets/js/vendor/';
+
+glob('node_modules/honeycomb/**/vendor/**/*.js', (err, files) => {
+    fs.ensureDir(vendorDir, err => {
+        if(err) {
+            console.error("Error creating vendor JS directory", err);
+            return;
+        }
+
+        files.forEach(file => {
+            fs.copy(file, path.join(vendorDir, path.basename(file)), err => {
+                if(err) {
+                    console.error("Error copying vendor JavaScript: ", err);
+                }
+            });
+        });
+    });
+});
