f2c98420f5c39141cf02e7691cc0344cdf200f17
Edward Casbon <edward@edwardcasbon.co.uk>
2017-10-17 15:28:50 +0100

    Added express app to serve website, and build script to automate build and open browser

---
+++ scripts/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const opn = require('opn');
+
+const app = express();
+const domain = 'http://localhost';
+const port = 3000;
+
+const dir = __dirname + '/../dist';
+const options = {
+    extensions: ['html']
+};
+
+app.use(express.static(dir, options));
+
+app.listen(port, () => {
+    console.log(`Serving the Honeycomb website at '${domain}' on port ${port}`);
+    console.log(`Opening ${domain}:${port}`);
+    opn(`http://localhost:${port}`);
+});
