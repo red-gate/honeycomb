79a1f5d6f58e65cfae08d6d3a00b243c5c52456d
philscott-rg <philip.scott@red-gate.com>
2024-09-04 11:40:02 +0100

    replace deprecated sass.render method with sass.compileAsync

--- scripts/honeycomb.css.js
+++ scripts/honeycomb.css.js
@@ -8,25 +8,21 @@ const fonts = [
 const fontsDir = "dist/assets/fonts/";
 
 // Compile Sass.
-sass.render({
-    file: "src/assets/css/honeycomb.scss",
-    includePaths: [ "node_modules" ],
-    outputStyle: "compressed",
-    outFile
-}, (err, result) => {
-    if(err) {
-        console.log("Error compiling Sass");
-        console.log(err);
-    } else {
-        fs.writeFile(outFile, result.css, (err) => {
-            if(err) {
-                console.log("Error writing CSS file");
-                console.log(err);
-            } else {
-                console.log(`CSS file compiled to ${outFile}`);
-            }
-        });
-    }   
+sass.compileAsync("src/assets/css/honeycomb.scss", {
+    loadPaths: [ "node_modules" ],
+    style: "compressed"
+}).then(result => {
+    fs.writeFile(outFile, result.css, (err) => {
+        if(err) {
+            console.log("Error writing CSS file");
+            console.log(err);
+        } else {
+            console.log(`CSS file compiled to ${outFile}`);
+        }
+    });
+}).catch(err => {
+    console.log("Error compiling Sass");
+    console.log(err);
 });
 
 // Copy fonts.
